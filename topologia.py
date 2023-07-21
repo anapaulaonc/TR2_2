c1f578780b9d4978bfe60b084eb5382943dbe969
anapaulaonc <anapaulaoncosta@gmail.com>
2023-07-21 16:49:35 -0300

    adicionando uma regra de fluxo

--- topologia.py
+++ topologia.py
@@ -3,6 +3,39 @@ from mininet.node import Controller, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import set_ev_cls
+
+#criando a classe  do aplicativo Ryu que sera responsavel por manipular as regras OpenFlow
+class SecurityApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SecurityApp, self).__init__(*args, **kwargs)
+
+    def switch_features_handler(self, ev):
+        #definindo o switch e o protocolo OpenFlow
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        #adicionando uma regra de fluxo para bloquear pacotes ICMP(ping) de h1 para h2 no switch s1
+        if datapath.id == 1:
+            match = parser.OFPMatch(eth_type=0x0800, ip_proto=1, ipv4_src='10.0.0.1', ipv4_dst='10.0.0.2')
+            actions = [] #sem acoes, ou seja, bloquear
+            self.add_flow(datapath, 1, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+
 
 #criando uma classe chamada CustomTopology
 class CustomTopology:
@@ -21,15 +54,15 @@ class CustomTopology:
 
         # Adicione quatro switches OpenFlow habilitados
         for i in range(1, 5):
-            switch_name = f's{i}'
+            switch_name =  's{}'.format(i)
             switch = self.net.addSwitch(switch_name)
             #armazenando os switches em um dicionario
             self.switches[switch_name] = switch
 
         #adicionando dez hosts
         for i in range(1, 11):
-            host_name = f'h{i}'
-            host_ip = f'10.0.0.{i}'
+            host_name =  'h{}'.format(i)
+            host_ip = '10.0.0.%d' % i
             host = self.net.addHost(host_name, ip=host_ip)
             #armazenando os hosts em um dicionario
             self.hosts[host_name] = host
@@ -46,15 +79,22 @@ class CustomTopology:
         self.net.start()
         #executando um teste de ping para garantir que todos hosts estao alcancaveis
         self.net.pingAll()
+
+        ryu_app = SecurityApp()
+        ryu_app.start([self])
+
         #iniciando a interface da linha de comando do Mininet
         CLI(self.net)
         #parando a rede do Mininet
         self.net.stop()
 
-if __name__ == '__main__':
 
-    setLogLevel('info')
-    #criando uma instancia da classe CustomTopology
-    topology = CustomTopology()
-    #chamando o metodo create_topology para criar a topologia e interagir com a rede
-    topology.create_topology()
+    
+            
+
+
+setLogLevel('info')
+#criando uma instancia da classe CustomTopology
+topology = CustomTopology()
+#chamando o metodo create_topology para criar a topologia e interagir com a rede
+topology.create_topology()
